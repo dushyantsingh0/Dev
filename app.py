410f0eda12f1f8954674f08fc5788ba92c4e5983
Dushyant <dushyant.cubastion@gmail.com>
2023-07-02 01:23:30 +0530

    streamlit_app

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.title("Hello Golu")
+    
+    # Add your Streamlit app code here
+    st.write("Welcome to my app!")
+    
+if __name__ == "__main__":
+    main()
