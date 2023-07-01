8881f1dc306b4bbb6cb61a7d576ae0c21cfd7221
Dushyant <dushyant.cubastion@gmail.com>
2023-07-02 02:27:34 +0530

    updated streamlit message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def main():
     
     # Add your Streamlit app code here
     st.write("Good morning Daddy's Girl")
-    st.st_canvas()
+    st.st_canvas(fillcolor="rgba(255,265,0,0.5)")
     
 if __name__ == "__main__":
     main()
