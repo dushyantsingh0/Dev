d22f707ffb8dba2d0e1897ceaecbc83ba88df6ae
Dushyant <dushyant.cubastion@gmail.com>
2023-07-02 02:41:51 +0530

    updated streamlit message

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ def main():
     st.write("Good morning Daddy's Girl")
     image = st.camera_input("Take a picture")
 
-    img = Image.open(image)
+    if image:
+        
+        img = Image.open(image)
 
-    grey_scale = img.convert("L")
+        grey_scale = img.convert("L")
 
-    st.image(grey_scale)
+        st.image(grey_scale)
 
 
 
