4d88ed412279ae5906f42ccd61a415f5913d0375
Dushyant <dushyant.cubastion@gmail.com>
2023-07-02 02:39:47 +0530

    updated streamlit message

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import streamlit as st
+from PIL import Image
 
 def main():
     st.title("Hello Golu")
     
     # Add your Streamlit app code here
     st.write("Good morning Daddy's Girl")
-    st.camera_input("Take a picture")
+    image = st.camera_input("Take a picture")
+
+    img = Image.open(image)
+
+    grey_scale = img.convert("L")
+
+    st.image(grey_scale)
 
 
 
